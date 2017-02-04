2c1bac67248d5183236971e7b567b642d641a544
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2017-02-04 15:22:06 -0600

    static files for admin

--- test51/settings.py
+++ test51/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # WYSIWYG SETTINGS
 REDACTOR_OPTIONS = {'lang': 'en', 'plugins': ['table']}
